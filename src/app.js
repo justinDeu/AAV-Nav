d158d8a9f60820951c80e42fb7804775bd3ddd55
Justin Deutsch <jusdeutsch22@gmail.com>
2019-03-15 20:19:59 -0400

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// Imports essentially, require libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Setting up the server
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, '/public')))
+app.set('view engine', 'ejs');
+
+app.get('', (req, res) => {
+    res.render('index');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
