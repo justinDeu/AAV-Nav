ddc2e5338cdbccfb6be126e8fa1005de992289e9
Justin Deutsch <jusdeutsch22@gmail.com>
2019-03-18 16:29:44 -0400

    Converting to routes.js

--- src/app.js
+++ src/app.js
@@ -10,9 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, '/public')))
 app.set('view engine', 'ejs');
 
-app.get('', (req, res) => {
-    res.render('index');
-});
+require('./routes')(app);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
