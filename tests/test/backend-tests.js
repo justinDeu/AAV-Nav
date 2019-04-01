ca3d5b966dd1e1b4a9ab1b7e6d03c1234111e09b
Justin Deutsch <jusdeutsch22@gmail.com>
2019-04-01 17:01:12 -0400

    Removing outer Describe in testing

--- tests/test/backend-tests.js
+++ tests/test/backend-tests.js
@@ -12,60 +12,57 @@ const get = (url) => chai.request(API_BASE).get(`${url}`);
 const post = (url, data) => chai.request(API_BASE).post(`${url}`).send(data);
 const query = (url, data) => chai.request(API_BASE).get(`${url}`).query(data);
 
-describe('/api', () => {
+describe('GET /api/arr', () => {
 
-    describe('GET /api/arr', () => {
-
-        it('should get empty array when nothing exists', async() => {
-            const res = await get('/api/arr');
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-            res.body.length.should.be.eql(0);
-            expect(res).to.have.header('content-type', /application\/json/);
-        });
-
-        it('should return 404 when requesting index out of range', async() => {
-            const res = await query('/api/arr', {index: 3});
-            res.should.have.status(404);
-        });
-
-        it('should return object at index', async() => {
-            // Adding two objects to the list 
-            let res = await addPoint({
-                lat: 1,
-                long: 11,
-                height: 111,
-                index: 0
-            });
-            res.body.length.should.be.eql(1);
-
-            res = await addPoint({
-                lat: 2,
-                long: 22,
-                height: 222,
-                index: 1
-            });
-            res.body.length.should.be.eql(2);
-
-            res = await query('/api/arr', {index: 0});
-            res.body.should.be.a('object');
-        
-            const body = res.body;
-            expect(body.lat, 'should have a latitude').to.equal(1);
-            expect(body.long, 'should have a longitude').to.equal(11);
-            expect(body.height, 'should have a height').to.equal(111);
-
-        });
+    it('should get empty array when nothing exists', async() => {
+        const res = await get('/api/arr');
+        res.should.have.status(200);
+        res.body.should.be.a('array');
+        res.body.length.should.be.eql(0);
+        expect(res).to.have.header('content-type', /application\/json/);
     });
 
-    describe('POST /api/arr', () => {
-        // no testing implemented yet
+    it('should return 404 when requesting index out of range', async() => {
+        const res = await query('/api/arr', {index: 3});
+        res.should.have.status(404);
     });
 
+    it('should return object at index', async() => {
+        // Adding two objects to the list 
+        let res = await addPoint({
+            lat: 1,
+            long: 11,
+            height: 111,
+            index: 0
+        });
+        res.body.length.should.be.eql(1);
 
-    describe('DELETE /api/arr', () => {
-        // no testing implemented yet
+        res = await addPoint({
+            lat: 2,
+            long: 22,
+            height: 222,
+            index: 1
+        });
+        res.body.length.should.be.eql(2);
+
+        res = await query('/api/arr', {index: 0});
+        res.body.should.be.a('object');
+    
+        const body = res.body;
+        expect(body.lat, 'should have a latitude').to.equal(1);
+        expect(body.long, 'should have a longitude').to.equal(11);
+        expect(body.height, 'should have a height').to.equal(111);
+
     });
+});
+
+describe('POST /api/arr', () => {
+     
+});
+
+
+describe('DELETE /api/arr', () => {
+    // no testing implemented yet
 });
 
 async function addPoint(data) {
